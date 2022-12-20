cf7a43c31d7927b0617cbe121edcd5ddbcd79497
Boyan Velikov <boyanvelikov@gmail.com>
2022-12-20 08:51:12 +0200

    day2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Link, useHistory } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const history = useHistory();
+
+  const subscribe = () => {
+    
+    // some log, maybe call apis
+
+    history.push('/channel');
+  }
+  
   return (
     <div className="App">
-      <nav></nav>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
 
       <div className='content'>React Router</div>
     </div>
